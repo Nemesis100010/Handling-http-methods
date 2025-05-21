9416ba5e05bcb1b8246953f7e7a241fd723cbc69
Koustuve Shyamal <koustuve30@gmail.com>
2025-05-21 18:36:56 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,71 @@
+import express from 'express'
+import 'dotenv/config'
+import logger from './logger.js';
+import morgan from 'morgan';
+
+const app = express()
+const port =  process.env||3000
+app.use(express.json())
+
+const morganFormat = ':method :url :status :response-time ms';
+
+app.use(morgan(morganFormat, {
+    stream: {
+        write: (message)=>{
+            const logObject = {
+                method: message.split(' ')[0],
+                url: message.split(' ')[1],
+                status: message.split(' ')[2],
+                responseTime: message.split(' ')[3]
+            };
+            logger.info(JSON.stringify(logObject));
+        }
+    }
+}))
+
+let teaData = []
+let nextId = 1
+// post tea data
+app.post('/teas', (req, res)=>{
+    const {name , price} =req.body
+    let newTea = {id : nextId++ , name , price }
+    teaData.push(newTea)
+    res.status(201).send(newTea)
+})
+//get all the tea data
+app.get('/teas', (req, res) =>{
+    res.status(200).send(teaData)
+})
+// get a tea with id
+app.get('/teas/:id' , (req, res) =>{
+   const tea =  teaData.find(t => t.id === parseInt(req.params.id));
+   if(!tea){
+     return res.status(404).send("tea not found !")
+   }
+   res.status(200).send(tea)
+})
+// update the tea
+app.put('/teas/:id' , (req, res) => {
+    const tea = teaData.find(t => t.id === parseInt(req.params.id))
+    if(!tea){
+        return res.status(404).send("tea not found !")
+    }
+    const {name , price} = req.body
+    tea.name = name
+    tea.price = price
+    res.status(200).send(tea)
+})
+// delete the tea
+app.delete('/teas/:id', (req, res) =>{
+    const index = teaData.findIndex(t => t.id === parseInt(req.params.id))
+    if (index === -1) {
+        res.status(404).send("Tea not found !")
+    }
+    teaData.splice(index , 1)
+
+    res.status(204).send("Tea deleted successfully")
+})
+
+app.listen(port , ()=>{
+    console.log(`Server is running at port: ${port}...`)
+})
